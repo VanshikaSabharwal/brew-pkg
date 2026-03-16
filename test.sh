972f9d7b07333ff3546bbd7d14e7cdad7f9e1641
VanshikaSabharwal <vanshikasabharwal06@gmail.com>
2026-03-16 12:18:00 +0530

    fixed test.sh to see symlinks logs

--- test.sh
+++ test.sh
@@ -3,15 +3,15 @@ set -euxo pipefail
 
 # Install latest brew
 if [[ $(command -v brew) == "" ]]; then
-    echo "Installing brew in order to build MetaCall"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo "Installing brew in order to build MetaCall"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Install brew-pkg
 brew tap --verbose metacall/brew-pkg
 brew install --verbose --HEAD metacall/brew-pkg/brew-pkg
 
-# overwrite installed brew-pkg with your local debug version
+# Overwrite installed brew-pkg with your local debug version
 BREWPKG_PATH=$(brew --prefix)/Cellar/brew-pkg/*/bin/brew-pkg.rb
 echo "Overwriting brew-pkg script at: $BREWPKG_PATH"
 cp brew-pkg.rb $BREWPKG_PATH
@@ -21,18 +21,32 @@ brew install python@3.12
 brew pkg --name python --with-deps --compress python@3.12
 test -f python.tar.gz
 test -f python.pkg
-# tar -ztvf python.tar.gz
+echo "=== Contents of python.tar.gz ==="
+tar -ztvf python.tar.gz
 
 brew pkg --name python-without-deps --compress python@3.12
 test -f python-without-deps.tar.gz
 test -f python-without-deps.pkg
-# tar -ztvf python-without-deps.tar.gz
+echo "=== Contents of python-without-deps.tar.gz ==="
+tar -ztvf python-without-deps.tar.gz
 
 brew install ruby@3.3
 brew pkg --name ruby-with-python --compress --relocatable --additional-deps python@3.12 ruby@3.3
 test -f ruby-with-python.tar.gz
 test -f ruby-with-python.pkg
-# tar -ztvf ruby-with-python.tar.gz
+echo "=== Contents of ruby-with-python.tar.gz ==="
+tar -ztvf ruby-with-python.tar.gz
 
 # Debug files and sizes
-ls -lh
+echo "=== Final package sizes ==="
+ls -lh python.tar.gz python.pkg python-without-deps.tar.gz python-without-deps.pkg ruby-with-python.tar.gz ruby-with-python.pkg
+
+# Verify symlinks exist in the lib directory inside each tarball
+echo "=== Verifying lib symlinks in python.tar.gz ==="
+tar -ztvf python.tar.gz | grep "opt/homebrew/lib" || echo "WARN: No symlinks found under opt/homebrew/lib in python.tar.gz"
+
+echo "=== Verifying lib symlinks in python-without-deps.tar.gz ==="
+tar -ztvf python-without-deps.tar.gz | grep "opt/homebrew/lib" || echo "WARN: No symlinks found under opt/homebrew/lib in python-without-deps.tar.gz"
+
+echo "=== Verifying lib symlinks in ruby-with-python.tar.gz ==="
+tar -ztvf ruby-with-python.tar.gz | grep "opt/homebrew/lib" || echo "WARN: No symlinks found under opt/homebrew/lib in ruby-with-python.tar.gz"
